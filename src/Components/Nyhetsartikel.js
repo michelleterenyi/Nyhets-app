79a008f2e7b4d6a53069aa31e462dabc868031ac
Michelle Terenyi <miselke98@gmail.com>
2019-05-28 20:53:36 +0200

    Initial Commit from Create React App

---
+++ src/Components/Nyhetsartikel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Nyhetsartikel() {
+    return (
+    <article>
+        <img src="https://source.unsplash.com/random/400x250/?pink" />
+        <h2>Test Nyhet</h2>
+        <p>Beskrivning av nyhet</p>
+        <a>Läs mer...</a>
+      </article>);
+}
+
+
+export default Nyhetsartikel;
