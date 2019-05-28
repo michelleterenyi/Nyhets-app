6fe364253217e03280e4393eb8117fb0222e02d8
Michelle Terenyi <miselke98@gmail.com>
2019-05-28 20:52:34 +0200

    ändrat

--- src/Components/Nyhetslista.js
+++ src/Components/Nyhetslista.js
@@ -1,38 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import Nyhetsartikel from './Nyhetsartikel';
 
 function Nyhetslista() {
-    return(
-    
+    return (
     <section>
-        <article>
-          <img src="https://source.unsplash.com/random/400x250/?pink" />
-          <h2>Test Nyhet</h2>
-          <p>Beskrivning av nyhet</p>
-          <a>Läs mer...</a>
-        </article>
-  
-        <article>
-          <img src="https://source.unsplash.com/random/400x250/?cat" />
-          <h2>Test Nyhet</h2>
-          <p>Beskrivning av nyhet</p>
-          <a>Läs mer...</a>
-        </article>
-  
-        <article>
-          <img src="https://source.unsplash.com/random/400x250/?sea" />
-          <h2>Test Nyhet</h2>
-          <p>Beskrivning av nyhet</p>
-          <a>Läs mer...</a>
-        </article>
-  
-        <article>
-          <img src="https://source.unsplash.com/random/400x250/?love" />
-          <h2>Test Nyhet</h2>
-          <p>Beskrivning av nyhet</p>
-          <a>Läs mer...</a>
-        </article>
+        <Nyhetsartikel />
     </section>);
 }
 
