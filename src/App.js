163470dc34d78a62dfb18d296943d909097b469b
Michelle Terenyi <miselke98@gmail.com>
2019-05-28 17:07:35 +0200

    ändrat

--- src/App.js
+++ src/App.js
@@ -4,39 +4,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nyhetslista from './Components/Nyhetslista';
 
-function App() {
-  return (
-    <section>
-      <article>
-        <img src="https://source.unsplash.com/random/400x250/?pink" />
-        <h2>Test Nyhet</h2>
-        <p>Beskrivning av nyhet</p>
-        <a>Läs mer...</a>
-      </article>
-
-      <article>
-        <img src="https://source.unsplash.com/random/400x250/?cat" />
-        <h2>Test Nyhet</h2>
-        <p>Beskrivning av nyhet</p>
-        <a>Läs mer...</a>
-      </article>
-
-      <article>
-        <img src="https://source.unsplash.com/random/400x250/?sea" />
-        <h2>Test Nyhet</h2>
-        <p>Beskrivning av nyhet</p>
-        <a>Läs mer...</a>
-      </article>
-
-      <article>
-        <img src="https://source.unsplash.com/random/400x250/?love" />
-        <h2>Test Nyhet</h2>
-        <p>Beskrivning av nyhet</p>
-        <a>Läs mer...</a>
-      </article>
-    </section>
-  );
+class App extends Component {
+  render () {
+    return (
+      <Nyhetslista />
+    );
+  }
 }
 
 export default App;
