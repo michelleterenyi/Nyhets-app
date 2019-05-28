dbdb500d3465bc46a3443bba9de89b429ba4a3b3
Michelle Terenyi <miselke98@gmail.com>
2019-05-28 17:07:55 +0200

    ändrat

---
+++ src/Components/Nyhetslista.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Nyhetslista() {
+    return(
+    
+    <section>
+        <article>
+          <img src="https://source.unsplash.com/random/400x250/?pink" />
+          <h2>Test Nyhet</h2>
+          <p>Beskrivning av nyhet</p>
+          <a>Läs mer...</a>
+        </article>
+  
+        <article>
+          <img src="https://source.unsplash.com/random/400x250/?cat" />
+          <h2>Test Nyhet</h2>
+          <p>Beskrivning av nyhet</p>
+          <a>Läs mer...</a>
+        </article>
+  
+        <article>
+          <img src="https://source.unsplash.com/random/400x250/?sea" />
+          <h2>Test Nyhet</h2>
+          <p>Beskrivning av nyhet</p>
+          <a>Läs mer...</a>
+        </article>
+  
+        <article>
+          <img src="https://source.unsplash.com/random/400x250/?love" />
+          <h2>Test Nyhet</h2>
+          <p>Beskrivning av nyhet</p>
+          <a>Läs mer...</a>
+        </article>
+    </section>);
+}
+
+export default Nyhetslista;
