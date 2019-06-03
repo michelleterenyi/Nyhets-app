b96c6a783cd4e21a89c2787107d8c8d538839fb2
Michelle Terenyi <miselke98@gmail.com>
2019-06-03 17:34:47 +0200

    ändrat

--- src/App.js
+++ src/App.js
@@ -8,9 +8,25 @@ import Nyhetslista from './Components/Nyhetslista';
 import data from './fuskdata';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {articles: [
+      
+      {urlToImage:"https://www.idg.se/editorial/1200/path/1.719785.1559553565!imageUploader/3272512706.jpg"
+    ,
+    title:"Testnyhet 1",
+    description:"Beskrivning av testnyheten",
+    },
+    
+      {urlToImage:"https://www.idg.se/editorial/1200/path/1.719785.1559553565!imageUploader/3272512706.jpg"
+    ,
+    title:"Testnyhet 2",
+    description:"Beskrivning av den andra testnyheten",
+    }]};
+  }
   render () {
     return (
-      <Nyhetslista />
+      <Nyhetslista artikellista={this.state.articles}/>
     );
   }
 }
